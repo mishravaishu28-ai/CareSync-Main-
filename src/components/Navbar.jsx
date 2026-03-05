23df6f7be6e1303f0bb81d7464e29160ecc8227d
Vaishnavi Mishra <mishra.vaishu28@gmail.com>
2026-03-05 16:56:30 +0530

    starting fresh on new branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 export default function Navbar() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t p-4 flex justify-around shadow-lg">
+    <nav className="fixed bottom-0 left-0 right-0 bg-pink-700 border-t p-4 flex justify-around shadow-lg">
       <span className="text-blue-600 font-bold cursor-pointer">Home</span>
-      <span className="text-gray-500 cursor-pointer">Hospitals</span>
-      <span className="text-gray-500 cursor-pointer">SOS</span>
+      <span className="text-white cursor-pointer">Hospitals</span>
+      <span className="text-white cursor-pointer">SOS</span>
     </nav>
+  
   );
 }
